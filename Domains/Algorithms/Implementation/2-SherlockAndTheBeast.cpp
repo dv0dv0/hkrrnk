a417c139b16845c28eff1f99c76cb69f34aa2499
dv0dv0 <dv0.org@gmail.com>
2016-03-04 11:43:52 -0800

    finished sherlock

--- Domains/Algorithms/Implementation/2-SherlockAndTheBeast.cpp
+++ Domains/Algorithms/Implementation/2-SherlockAndTheBeast.cpp
@@ -3,19 +3,67 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+
+#define UNDEF   "-1"
+#define THREE   "555"
+#define FIVE    "33333"
+#define SIX     "555555"
+#define EIGHT   "55533333"
+#define NINE    "555555555"
+#define TEN     "3333333333"
+
 using namespace std;
 
+void lastTen(int n);
 
 int main(){
     int t;
+    int i;
     cin >> t;
     for(int a0 = 0; a0 < t; a0++){
         int n;
         cin >> n;
+        i = n;
+
+        //      check if 1 - 10
+        if (n < 11) {
+            lastTen (n);
+        } else {
+            while (i > 10) {
+                cout << "555";
+                i -= 3;
+            }
+            lastTen(i);
+        }
+
     }
     return 0;
 }
 
+void lastTen (int n) {
 
-/*
- * build vector of the numbe
+    switch (n) {
+        case 3:
+            cout << THREE << endl;
+            break;
+        case 5:
+            cout << FIVE << endl;
+            break;
+        case 6:
+            cout << SIX << endl;
+            break;
+        case 8:
+            cout << EIGHT << endl;
+            break;
+        case 9:
+            cout << NINE << endl;
+            break;
+        case 10:
+            cout << TEN << endl;
+            break;
+        default:
+            cout << UNDEF << endl;
+            break;
+    }
+    return;
+}
