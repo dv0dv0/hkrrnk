32f57ecdfa8c8dddcf7709b9fdc6d0e660a8c3ca
Devin Valdez <devin@Devins-MacBook-Pro.local>
2016-03-03 20:38:52 -0800

    finished angry prof

--- Domains/Algorithms/Implementation/1-AngryProfessor.cpp
+++ Domains/Algorithms/Implementation/1-AngryProfessor.cpp
@@ -7,15 +7,34 @@ using namespace std;
 
 int main(){
     int t;
+    int ots;
     cin >> t;
+
+//  loop per test case
     for(int a0 = 0; a0 < t; a0++){
         int n;
         int k;
+        ots = 0;
         cin >> n >> k;
         vector<int> a(n);
+
+ //     loop per element in vector
         for(int a_i = 0;a_i < n;a_i++){
             cin >> a[a_i];
+
+  //        if student on time      
+            if (a[a_i] <= 0) {
+                ots += 1;
+            }
+
         }
+        if (ots < k) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+
     }
     return 0;
 }
+
