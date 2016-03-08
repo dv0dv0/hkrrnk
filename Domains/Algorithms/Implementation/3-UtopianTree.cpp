603c72d73f5562996c3d1c16b75ecb4f780e30ce
dv0dv0 <dv0.org@gmail.com>
2016-03-07 16:26:32 -0800

    finished Utopian tree

--- Domains/Algorithms/Implementation/3-UtopianTree.cpp
+++ Domains/Algorithms/Implementation/3-UtopianTree.cpp
@@ -10,8 +10,19 @@ int main(){
     int t;
     cin >> t;
     for(int a0 = 0; a0 < t; a0++){
-        int n;
+        int n, x;
         cin >> n;
+
+        if (n == 0) {
+            cout << 1 << endl;
+        } else if ( n % 2 == 0 ) {
+            n += 1;
+            x = pow(2, ((n+1)/2)) - 1;
+            cout << x << endl;
+        } else {
+            x = 2 * (pow(2, ((n+1)/2)) - 1);
+            cout << x << endl;
+        }
     }
     return 0;
 }
