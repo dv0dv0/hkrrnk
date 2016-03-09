6dfda1bdca0c337cf738250c099fe4bc04059125
dv0dv0 <dv0.org@gmail.com>
2016-03-09 12:55:02 -0800

    made additions to find digits

--- Domains/Algorithms/Implementation/4-FindDigits.cpp
+++ Domains/Algorithms/Implementation/4-FindDigits.cpp
@@ -9,8 +9,22 @@ int main(){
     int t;
     cin >> t;
     for(int a0 = 0; a0 < t; a0++){
-        int n;
+        int n, L;
+        string nString;
         cin >> n;
+
+//      convert num to string, then find length
+        ostringsstream convert;
+        convert << n;
+        nString = convert.str();
+        L = nString.length();
+
+//      loop through string, check divisibility
+        for (int x = 0; x < L; x++) {
+            
+
+        }
+
     }
     return 0;
 }
